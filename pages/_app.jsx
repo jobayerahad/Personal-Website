98161e15cad91e5beedc8d31f4c19abdc1c5e90f
Md Jobayer Al Mahmud Ahad <30753487+jobayerahad@users.noreply.github.com>
2023-04-17 00:35:47 +0600

    added acme chal

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,17 @@
 import { MantineProvider } from '@mantine/core'
-import { NotificationsProvider } from '@mantine/notifications'
+import { Notifications } from '@mantine/notifications'
 import { ModalsProvider } from '@mantine/modals'
 
 import '@styles/main.scss'
 import theme from '@config/theme'
 
 const App = ({ Component, pageProps }) => (
-  <MantineProvider theme={theme}>
-    <NotificationsProvider>
-      <ModalsProvider>
-        <Component {...pageProps} />
-      </ModalsProvider>
-    </NotificationsProvider>
+  <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+    <Notifications />
+
+    <ModalsProvider>
+      <Component {...pageProps} />
+    </ModalsProvider>
   </MantineProvider>
 )
 
